9c511d13c1d23fa5d6981c4f95a2db9b72481106
Kimberly Howard <Kimberly Howard>
2018-07-16 16:59:30 -0600

    update js

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -6,14 +6,21 @@ module.exports = function (app) {
 		res.json(friends);
 	});
 
-	app.post('/api/friends', function(req, res){
-		var userInput = req.body;
-		friends.push(userInput);
-		res.json(friends);
-
-		var userResponses = userInput.scores;
-		console.log('User Score = ' + userResponses);
+	app.post("/api/friends", function (req, res) {
+		var friendIndex = 0;
+		var prevDiff = 50;
+		for (let i = 0; i < friends.length; i++) {
+			var newDiff = 0;
+			for (let j = 0; j < 10; j++) {
+				newDiff += Math.abs(friends[i].scores[j] - req.body.scores[j]);
+			}
+			if (newDiff < prevDiff) {
+				prevDiff = newDiff;
+				friendIndex = i;
+			}
+		}
+		friends.push(req.body);
+		res.json(friends[friendIndex]);
 
 	});
-
 }
