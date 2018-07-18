615e4ccc1fe9e34a7590344b4ee5638aa314da7d
Kimberly Howard <Kimberly Howard>
2018-07-17 23:10:11 -0600

    changes to js

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,5 +5,11 @@ var friendsArray = [
 		score: [2,1,3,1,5,4,3,2,5,5]
 	}
 ];
+var friends = [{
+	"name":"Cookie Monster",
+	"photo":"https://imagesvc.timeincapp.com/v3/mm/image?url=http%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fcookie-monster-bakes-cookies-ft-blog1116.jpg%3Fitok%3D5cjKiFp0&w=700&q=85",
+	"scores":[5,1,4,4,5,2,2,5,4,1]
+  }
+];
 
-module.exports = friendsArray;
+module.exports = friends;
--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -1,26 +1,54 @@
-var path = require('path');
-var friends = require('../data/friends.js');
+var friendData = require("../data/friends");
 
-module.exports = function (app) {
-	app.get('/api/friends', function(req, res){
-		res.json(friends);
-	});
+module.exports = function(app) {
 
-	app.post("/api/friends", function (req, res) {
-		var friendIndex = 0;
-		var prevDiff = 50;
-		for (let i = 0; i < friends.length; i++) {
-			var newDiff = 0;
-			for (let j = 0; j < 10; j++) {
-				newDiff += Math.abs(friends[i].scores[j] - req.body.scores[j]);
-			}
-			if (newDiff < prevDiff) {
-				prevDiff = newDiff;
-				friendIndex = i;
-			}
-		}
-		friends.push(req.body);
-		res.json(friends[friendIndex]);
+  app.get("/api/friends", function(req, res) {
+    res.json(friendData);
+  });
 
-	});
-}
+  app.post("/api/friends", function(req, res) {
+
+    
+    console.log(req.body.scores);
+
+    var highest;
+	var highDifference = 100;
+    var compatible;
+    var loopDifference = 0;
+
+    // loop through each friend
+    for (var i = 0; i < friendData.length; i++) {
+
+      // loop through each friends scores
+      for (var j = 0; j < friendData[i].scores.length; j++) {
+        
+        // find difference amount
+        loopDifference += Math.abs(req.body.scores[j] - friendData[i].scores[j])
+      }
+
+      // // if the total Difference was lower for this friend, then change the highest friend
+      if (loopDifference < highDifference) {
+        // changes the best match
+        highDifference = loopDifference;
+        
+        compatible = i;
+      }
+
+      // reset loop difference
+      loopDifference = 0;
+    }
+
+    console.log(compatible);
+    console.log(friendData[compatible].name);
+    
+    res.json(friendData[compatible]);
+    friendData.push(req.body);
+
+  });
+
+  app.post("/api/clear", function() {
+    // Empty out the arrays of data
+    friendData = [];
+    res.json(friendData);
+  });
+};
